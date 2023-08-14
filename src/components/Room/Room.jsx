e5e25f25185c13693f5c0baf264118059f42fd49
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-14 19:34:08 +0200

    Implement leaving, kicking from and joining a room

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { fetchRoom } from "../../../helpers/API";
+import { fetchJoinRoom, fetchLeaveRoom, fetchRoom } from "../../../helpers/API";
 import { useAuthStore } from "../../store";
 import Delete from "./Delete";
 import Update from "./Update";
@@ -11,23 +11,49 @@ function Room() {
     const [room, setRoom] = useState(null);
     const [isAdmin, setIsAdmin] = useState(false);
 
-    function setRoomName(name) {
-        setRoom((room) => ({ ...room, name }));
-    }
-
     useEffect(() => {
         async function getRoom() {
             const res = await fetchRoom(id);
 
             if (res.status === 200) {
-                if (user.id == res.data.room.admin._id) setIsAdmin(true);
+                if (user._id == res.data.room.admin._id) setIsAdmin(true);
 
                 setRoom(res.data.room);
             }
         }
 
         getRoom();
+
+        return () => {
+            setIsAdmin(false);
+        };
     }, [id]);
+
+    function setRoomName(name) {
+        setRoom((room) => ({ ...room, name }));
+    }
+
+    async function leaveRoom(userId) {
+        const res = await fetchLeaveRoom(id, userId);
+
+        if (res.status === 200) {
+            setRoom((room) => ({
+                ...room,
+                chatters: room.chatters.filter((chatter) => chatter._id !== userId),
+            }));
+        }
+    }
+
+    async function joinRoom() {
+        const res = await fetchJoinRoom(id);
+
+        if (res.status === 200) {
+            setRoom((room) => ({
+                ...room,
+                chatters: [...room.chatters, user],
+            }));
+        }
+    }
 
     if (room) {
         return (
@@ -38,11 +64,20 @@ function Room() {
                 </h2>
                 {isAdmin && <Delete />}
                 {isAdmin && <Update setRoomName={setRoomName} />}
+                {!isAdmin &&
+                    (room.chatters.some((chatter) => chatter._id === user._id) ? (
+                        <button onClick={() => leaveRoom(user._id)}>Leave Room</button>
+                    ) : (
+                        <button onClick={() => joinRoom()}>Join Room</button>
+                    ))}
                 <h3>Chatters</h3>
                 <ul>
                     {room.chatters.map((chatter) => (
                         <li key={chatter._id}>
                             <Link to={"/talk/users/" + chatter.username}>{chatter.username}</Link>
+                            {isAdmin && chatter._id !== room.admin._id && (
+                                <button onClick={() => leaveRoom(chatter._id)}>Kick</button>
+                            )}
                         </li>
                     ))}
                 </ul>
