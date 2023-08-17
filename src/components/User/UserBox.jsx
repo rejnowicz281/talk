10e44cb1106005ea420c36a151b3e7e7eddc552e
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-18 00:16:40 +0200

    Add admin tag if needed for UserBox

--- src/components/User/UserBox.jsx
+++ src/components/User/UserBox.jsx
@@ -2,16 +2,19 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./User.css";
 
-function UserBox({ user }) {
+function UserBox({ user, adminTag = false }) {
     return (
         <div className="userbox">
             <img height={50} width={50} src={user.avatar} alt="?" />
-            <Link to={"/talk/users/" + user.username}>{user.username}</Link>
+            <Link to={"/talk/users/" + user.username}>
+                {user.username} {adminTag && "(Admin)"}
+            </Link>
         </div>
     );
 }
 
 UserBox.propTypes = {
+    adminTag: PropTypes.bool,
     user: PropTypes.shape({
         username: PropTypes.string.isRequired,
         avatar: PropTypes.string.isRequired,
