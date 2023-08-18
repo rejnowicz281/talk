cf2e6464351b8e27b5346d9ff9d9e61a2403264b
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-08-18 16:12:28 +0200

    Initial gh-pages deployment

--- helpers/API.js
+++ helpers/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://talk.fly.dev/";
 
 const apiAuth = axios.create({
     baseURL: API_URL,
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:3000";
+const URL = "https://talk.fly.dev";
 
 const socket = io(URL);
 
