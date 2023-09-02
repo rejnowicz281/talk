7e89b887c64f53f2682ca04164c504a646449757
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-03 00:34:04 +0200

    Automatically make the main sidebar close when navigating to a new page

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useMainSidebarStore } from "../../store";
 import SideBar from "./SideBar";
 import css from "./styles/MainLayout.module.css";
 
 function MainLayout() {
+    const closeMainSidebar = useMainSidebarStore((state) => state.closeMainSidebar);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        closeMainSidebar(); // Close main sidebar when navigating to a new page
+    }, [navigate]);
+
     return (
         <div className={css.container}>
             <SideBar />
