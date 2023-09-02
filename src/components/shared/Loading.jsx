dca925b72fa4da3a03ffa5e37bb5e88db1af83c7
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-09-03 00:09:06 +0200

    Fix Loading component overflow issue

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 import css from "./styles/Loading.module.css";
 
 function Loading({ text }) {
-    return <div className={css.loading}>{text ? text : "Loading..."}</div>;
+    return (
+        <div className={css.wrapper}>
+            <div className={css.loading}>{text ? text : "Loading..."}</div>
+        </div>
+    );
 }
 
 Loading.propTypes = {
